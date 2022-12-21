a06b5b13d99ed3c80be2f0f9dec747e031b306d8
Jubo98 <julia.borghs@gmail.com>
2022-12-21 15:59:58 +0100

    Datenbank vollständig (bis auf Fotos)

---
+++ app/src/main/java/com/tudresden/fairlaufen/DescriptionActivity.java
@@ -0,0 +1,17 @@
+package com.tudresden.fairlaufen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
+
+//über select, cursor (Seite 3) in java activity
+// im layout verschiedene text views festlegen als Platzhalter
